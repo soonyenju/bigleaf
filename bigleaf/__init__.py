6b78288a8b25ea9fb125d2ed76142f17d28d4db0
Songyan Zhu <zhusy93@gmail.com>
2025-04-18 21:11:49 +0100

    Create __init__.py

---
+++ bigleaf/__init__.py
@@ -0,0 +1,9 @@
+"""
+Converting bigleaf in r to Python
+Credit: https://github.com/cran/bigleaf/tree/master/R
+It's still under debugging, use with cautions
+"""
+
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize_scalar
